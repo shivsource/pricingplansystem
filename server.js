3f22df49061987bf293ee9e7ddd6b11d433f118e
shivsource <120500436+shivsource@users.noreply.github.com>
2025-07-20 18:43:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
+
+
+
+app.use((err, req, res, next) => {
+  if (err) return res.status(500).json({ message: "Something went wrong" });
+  return next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
