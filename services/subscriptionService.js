1c64c44fe85ed9dad783d046ef27e6f907ad1733
shivsource <120500436+shivsource@users.noreply.github.com>
2025-07-20 18:40:41 +0530

    Update subscriptionService.js

--- services/subscriptionService.js
+++ services/subscriptionService.js
@@ -1,23 +1,36 @@
-import UserPlan from '../models/UserPlan.js';
+import UserPlan from '../models/userPlan.js';
 import Plan from '../models/Plan.js';
 import { cacheUserPlan } from './redisService.js';
+import { successRes } from '../utils/helper.js';
 
 export const subscribeUserToPlan = async (userId, planId) => {
-  const plan = await Plan.findById(planId);
-  const startDate = new Date();
-  const endDate = new Date();
-  endDate.setDate(startDate.getDate() + plan.durationDays);
+  try {
+    const plan = await Plan.findById(planId);
+    if (!plan) {
+      let err = new Error('Invalid planId');
+      err.statusCode = 400;
+      throw err;
+    }
 
-  const userPlan = await UserPlan.create({
-    userId,
-    planId,
-    startDate,
-    endDate,
-    isActive: true
-  });
+    const startDate = new Date();
+    const endDate = new Date();
+    endDate.setDate(startDate.getDate() + plan.durationDays);
 
-  await cacheUserPlan(userId, userPlan, plan.features, endDate);
-  return userPlan;
+    const userPlan = await UserPlan.create({
+      userId,
+      planId,
+      startDate,
+      endDate,
+      isActive: true
+    });
+
+    // await cacheUserPlan(userId, userPlan, plan.features, endDate);
+    return { name: plan.name, price: plan.price, durationDays: plan.durationDays, userPlan };
+  } catch (error) {
+    let err = new Error(`${error.message || 'Error while subscribing plan in subscriptionService.'}`);
+    err.statusCode = error.statusCode || 500;
+    throw err;
+  }
 };
 
 export const getUserPlan = async (userId) => {
