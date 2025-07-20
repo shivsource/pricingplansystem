4330223e0048987eb72511d2965b41181132d47e
shivsource <120500436+shivsource@users.noreply.github.com>
2025-07-20 18:31:59 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+    console.log('Connected to DB successfully.');
+  } catch (error) {
+    console.log('Failed to connect DB');
+  }
+}
+
+export default connectDb;
