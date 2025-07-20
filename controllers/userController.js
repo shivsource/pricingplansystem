b5dd712bf19b7417193b8e6988bdae728009effb
shivsource <120500436+shivsource@users.noreply.github.com>
2025-07-20 18:36:48 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,27 @@
 // userController.js
 import { subscribeUserToPlan, getUserPlan } from '../services/subscriptionService.js';
+import { errorRes, successRes } from '../utils/helper.js';
 
 export const subscribePlan = async (req, res) => {
-  const userId = req.user.id;
-  const { planId } = req.body;
-  const result = await subscribeUserToPlan(userId, planId);
-  res.json(result);
+  try {
+    const userId = req.user.id;
+    const { planId } = req.body;
+    const result = await subscribeUserToPlan(userId, planId);
+
+    return successRes(res, { message: `Successfully subscribed to Plan ${result.name}`, data: result });
+  } catch (error) {
+    return errorRes(res, error);
+  }
 };
 
 export const getActivePlan = async (req, res) => {
-  const plan = await getUserPlan(req.user.id);
-  res.json(plan);
+  try {
+    const plan = await getUserPlan(req.user.id);
+    if (!plan) return errorRes(res, { statusCode: 404, message: "No data found" })
+
+    return successRes(res, plan);    
+  } catch (error) {
+    return errorRes(res, error);
+  }
+
 };
